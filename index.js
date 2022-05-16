a6e245ea61999044bb8cfba02350ebbfdb6c0230
Mirwais Farahi <waisfarahi@gmail.com>
2022-05-16 13:15:15 +0500

    Edit index.js File

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const toggleButton = document.getElementById('toggle-btn');
 const navigationList = document.getElementById('navigation-list');
 
 toggleButton.addEventListener('click', () => {
-    navigationList.classList.toggle('active');
+  navigationList.classList.toggle('active');
 });
