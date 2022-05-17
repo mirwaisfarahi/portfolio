e9f4b61a135e0761609c9dd1f3e816a8b0277418
Mirwais Farahi <waisfarahi@gmail.com>
2022-05-17 09:25:08 +0500

    Delete index.js File

--- index.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('toggle-btn');
-const navigationList = document.getElementById('navigation-list');
-
-toggleButton.addEventListener('click', () => {
-  navigationList.classList.toggle('active');
-});
